6414b74d45540c3dcb9886b99495ac831e5395c1
Killian Callaghan <48124517+Killian-Callaghan@users.noreply.github.com>
2022-03-03 22:09:07 +0000

    Created stats.html and UserController.java

--- src/main/java/howdo/vaccine/repository/UserRepository.java
+++ src/main/java/howdo/vaccine/repository/UserRepository.java
@@ -2,9 +2,15 @@ package howdo.vaccine.repository;
 
 import howdo.vaccine.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     List<User> getUserByPpsNumber(String ppsNumber);
+
+    @Query("SELECT COUNT(id) FROM User")
+    int userTotal();
+
+
 }
