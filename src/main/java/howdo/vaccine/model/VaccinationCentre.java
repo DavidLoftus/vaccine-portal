1915b9105a45d0b2a7fe568fab4357bebc891b71
joecl <joseph.clarke@ucdconnect.ie>
2022-03-04 17:02:35 +0000

    Added a String name attribute and getters/setters to VaccinationCentre

--- src/main/java/howdo/vaccine/model/VaccinationCentre.java
+++ src/main/java/howdo/vaccine/model/VaccinationCentre.java
@@ -3,6 +3,7 @@ package howdo.vaccine.model;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
 import java.util.List;
 
 @Entity
@@ -11,7 +12,34 @@ public class VaccinationCentre {
     @Id
     private Long id;
 
+    @NotBlank
+    private String name;
+
     @OneToMany(mappedBy = "location")
     List<Appointment> appointments;
 
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Appointment> getAppointments() {
+        return appointments;
+    }
+
+    public void setAppointments(List<Appointment> appointments) {
+        this.appointments = appointments;
+    }
 }
