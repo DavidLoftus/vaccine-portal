918c4dd0d2a42ec97818d94caba102e023bc9c2c
joecl <joseph.clarke@ucdconnect.ie>
2022-03-04 15:47:42 +0000

    Added GET and POST endpoints to VaccineController.java

--- src/main/java/howdo/vaccine/controller/VaccineController.java
+++ src/main/java/howdo/vaccine/controller/VaccineController.java
@@ -1,11 +1,13 @@
 package howdo.vaccine.controller;
 
 import howdo.vaccine.model.Appointment;
+import howdo.vaccine.model.User;
 import howdo.vaccine.repository.UserRepository;
 import howdo.vaccine.repository.VaccineAptRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -22,10 +24,16 @@ public class VaccineController {
     @Autowired
     UserRepository userRepository;
 
-    @PostMapping(value = "/appointments", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-    public void appointmentsPost(HttpServletResponse response, @Valid @ModelAttribute Appointment appointment) throws IOException {
+    @GetMapping("/appointments")
+    public String registerGet(@ModelAttribute User user) {
+        return "registerVaccineAppt";
+    }
 
-        //TODO
+    @PostMapping(value = "/appointments", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public void appointmentsPost(HttpServletResponse response, @Valid @ModelAttribute Appointment appointment, @ModelAttribute User user) throws IOException {
+
+        //the appointment object contains a date and a vaccination centre from the form
+        appointment.setUser(user);
 
         vaccineAptRepository.save(appointment);
 
