1d84478dc849223c6da1286d22787738c895476c
joecl <joseph.clarke@ucdconnect.ie>
2022-03-10 18:20:13 +0000

    Changed date field from sql to util

--- src/main/java/howdo/vaccine/model/VaccineDose.java
+++ src/main/java/howdo/vaccine/model/VaccineDose.java
@@ -4,7 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import java.sql.Date;
+import java.util.Date;
 
 @Entity
 public class VaccineDose {
