4ce47cc193b46204a4bd36cc0f1eaef99cf4f2a5
joecl <joseph.clarke@ucdconnect.ie>
2022-03-10 17:08:31 +0000

    Added getters and setters to VaccineDose

--- src/main/java/howdo/vaccine/model/VaccineDose.java
+++ src/main/java/howdo/vaccine/model/VaccineDose.java
@@ -20,4 +20,44 @@ public class VaccineDose {
     private Integer dose;
 
     private String vaccineType;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Integer getDose() {
+        return dose;
+    }
+
+    public void setDose(Integer dose) {
+        this.dose = dose;
+    }
+
+    public String getVaccineType() {
+        return vaccineType;
+    }
+
+    public void setVaccineType(String vaccineType) {
+        this.vaccineType = vaccineType;
+    }
 }
