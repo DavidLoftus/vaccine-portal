1f7f73d05b2e2e38265ea5702032b79527e2fa53
joecl <joseph.clarke@ucdconnect.ie>
2022-03-10 20:36:15 +0000

    Added cascade to user_id key

--- src/main/java/howdo/vaccine/model/VaccineDose.java
+++ src/main/java/howdo/vaccine/model/VaccineDose.java
@@ -1,9 +1,6 @@
 package howdo.vaccine.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import java.util.Date;
 
 @Entity
@@ -12,7 +9,7 @@ public class VaccineDose {
     @GeneratedValue
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(cascade= CascadeType.ALL)
     private User user;
 
     private Date date;
