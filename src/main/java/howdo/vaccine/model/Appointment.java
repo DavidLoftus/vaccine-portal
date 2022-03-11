de0f9c998877379e16940918d6b14ff8fa3a3b6b
DavidLoftus <43099493+DavidLoftus@users.noreply.github.com>
2022-03-11 01:20:07 +0000

    Activity log

--- src/main/java/howdo/vaccine/model/Appointment.java
+++ src/main/java/howdo/vaccine/model/Appointment.java
@@ -3,16 +3,14 @@ package howdo.vaccine.model;
 import org.hibernate.annotations.Cascade;
 import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.util.Date;
 
 @Entity
 public class Appointment {
     @Id
+    @GeneratedValue
     private Long id;
 
     @NotNull
