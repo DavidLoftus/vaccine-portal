45eb399be309e4a55024b8b1b028723735cfda2a
joecl <joseph.clarke@ucdconnect.ie>
2022-03-11 12:36:11 +0000

    Added findByDate query

--- src/main/java/howdo/vaccine/repository/VaccineAptRepository.java
+++ src/main/java/howdo/vaccine/repository/VaccineAptRepository.java
@@ -3,5 +3,9 @@ package howdo.vaccine.repository;
 import howdo.vaccine.model.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Date;
+import java.util.List;
+
 public interface VaccineAptRepository extends JpaRepository<Appointment, Long> {
+    List<Appointment> findByDate(Date date);
 }
