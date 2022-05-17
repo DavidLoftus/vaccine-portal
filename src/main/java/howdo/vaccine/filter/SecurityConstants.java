75570d73434c49c302018b563250bf86b570e020
joecl <joseph.clarke@ucdconnect.ie>
2022-05-17 16:06:24 +0100

    Added constants file

---
+++ src/main/java/howdo/vaccine/filter/SecurityConstants.java
@@ -0,0 +1,7 @@
+package howdo.vaccine.filter;
+
+public class SecurityConstants {
+    public static final String SECRET = "n2r5u8x/A%D*G-KaPdSgVkYp3s6v9y$B&E(H+MbQeThWmZq4t7w!z%C*F-J@NcRf";
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String COOKIE_NAME = "JWT";
+}
